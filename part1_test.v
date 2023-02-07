714e2de9f1d5ff6618576529a7719a70723b7382
gsamtz <121076311+gsamtz@users.noreply.github.com>
2023-02-06 22:35:03 -0600

    Add code refactoring

--- part1_test.v
+++ part1_test.v
@@ -2,25 +2,24 @@
 module Breadboard (w, x, y, z, r0, r1, r2, r3, r4, r5, r6, r7, r9, r8);
 
 input w, x, y, z;
-output r0, r1, r2, r4, r5, r7, r3, r6,r9, r8;
-reg r0, r1, r2, r4, r5, r7, r3, r6,r9, r8;
+output r0, r1, r2, r3, r4, r5, r6, r7,r8, r9;
+reg r0, r1, r2, r3, r4, r5, r6, r7,r8, r9;
 wire w, x, y, z;
 
 always @ (w, x, y, z, r0, r1, r2, r3, r4, r5, r6, r7, r9, r8) begin
 
-r0 = (w & z | w & y | x & ~y & z | ~w & ~x & ~y & ~z);  // f0 of the bookwork
-r1 = (~x & z | ~y & z | ~y & ~w);
-r2 = (y | ~w) & (x | y | z) & (~x | ~z) & (x | z | ~w) & (w | ~y | ~z) & (w | ~x | ~y);
-r4 = (w & ~y) | (w & z & ~x) | (~x & ~y);
-r5 = (x & y) | (x & ~z) | (y & ~w & ~z) | (z & ~w & ~x & ~y);
-r7 = (~x & ~y & z)|(~w & x & y & ~z)|(w & ~x & y & ~z)|(w & x &y & z); 
-r3 = (~y | x & z);
-r6 = (w | ~y) & (~x | ~z) & (y | z | ~w); 
-r9 = (~w & ~x & ~y & ~z) | (~y & z & x) | (w & y & ~z) | (w & y & ~x) | (w & z & x) | (w & z & ~y);
-r8 = (x & ~z) | (~w & y & z) | (~x & ~y & z);
+    r0 = (w & z | w & y | x & ~y & z | ~w & ~x & ~y & ~z);
+    r1 = (~x & z | ~y & z | ~y & ~w);
+    r2 = (y | ~w) & (x | y | z) & (~x | ~z) & (x | z | ~w) & (w | ~y | ~z) & (w | ~x | ~y);
+    r4 = (w & ~y) | (w & z & ~x) | (~x & ~y);
+    r5 = (x & y) | (x & ~z) | (y & ~w & ~z) | (z & ~w & ~x & ~y);
+    r7 = (~x & ~y & z) | (~w & x & y & ~z) | (w & ~x & y & ~z) | (w & x &y & z); 
+    r3 = (~y | x & z);
+    r6 = (w | ~y) & (~x | ~z) & (y | z | ~w); 
+    r9 = (~w & ~x & ~y & ~z) | (~y & z & x) | (w & y & ~z) | (w & y & ~x) | (w & z & x) | (w & z & ~y);
+    r8 = (x & ~z) | (~w & y & z) | (~x & ~y & z);
 
 end
-
 endmodule
 
 //----------------------------------------------------------------------
@@ -33,7 +32,6 @@ reg y;
 reg z;
 
 wire f0, f1, f2, f4, f5, f7, f3, f6, f9, f8;
-
 
 Breadboard bb8(w, x, y, z, f0, f1, f2, f3, f4, f5, f6, f7, f9, f8);
 
@@ -49,13 +47,15 @@ initial begin
 
         #20;
 
-        $display ("|%4d|%3d|%3d|%3d|%3d| %3d| %3d| %3d| %3d| %3d| %3d| %3d| %3d| %3d| %3d|", i, w, x, y, z, f0, f1, f2, f3, f4, f5, f6, f7, f8, f9);
-		if (i % 4 == 3) //Every fourth row of the table, put in a marker for easier reading.
-		  $write ("|----+---+---+---+---+----+----+----+----+----+----+----+----+----+----+\n");//Write acts a bit like a java System.print
+        $display ("|%4d|%3d|%3d|%3d|%3d| %3d| %3d| %3d| %3d| %3d| %3d| %3d| %3d| %3d| %3d|", 
+            i, w, x, y, z, f0, f1, f2, f3, f4, f5, f6, f7, f8, f9);
+
+		if (i % 4 == 3) // Every fourth row of the table, put in a marker for easier reading.
+		  $write ("|----+---+---+---+---+----+----+----+----+----+----+----+----+----+----+\n");
     end
 
     #20;
     $finish;
+    
 end
-
 endmodule
